8a6842e55cf2a210c8502ac22240904e3556146d
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-05-04 17:27:44 -0500

    Adding getStudentsByCertification method to the StudentService class and respective tests

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -16,4 +16,37 @@ describe("Tests for student services", () => {
         const allStudents = StudentService.getAllStudents(students);
         expect(allStudents.length).toBe(3);
     });
+    test("3) Return students with certification", () => {
+        const students = [
+            { name: "Warren", email: "algo@algo.com", haveCertification: true},
+            { name: "Lucinda", email: "algo@algo.com", haveCertification: true},
+            { name: "Fuentes", email: "algo@algo.com", haveCertification: false}
+        ];
+        const allStudents = StudentService.getAllStudents(students);
+        const emailStudents = allStudents.filter(studentsByEmail => studentsByEmail.haveCertification == false);
+        expect(emailStudents.length).toBe(1);
+    });
+    test("4) Return students emails with certification == true", () => {
+        const students = Reader.readJsonFile("test/students.json");
+        const studentsWithCertification = StudentService.getStudentsByCertification(students);
+        expect(studentsWithCertification.length).toBe(2);
+    });
+    test("5) Return data of students with certification == true", () => {
+        const students = [
+            { name: "Warren", email: "algo@algo.com", haveCertification: false},
+            { name: "Lucinda", email: "algo@algo.com", haveCertification: true},
+            { name: "Fuentes", email: "algo@algo.com", haveCertification: false}
+        ];
+        const studentsWithCertification = StudentService.getStudentsByCertification(students);
+        expect(studentsWithCertification).not.toEqual([{ name: "Lucinda", email: "algo@algo.com", haveCertification: true}]);
+    });
+    test("5) Return email of students with certification == true", () => {
+        const students = [
+            { name: "Warren", email: "algo@algo.com", haveCertification: false},
+            { name: "Lucinda", email: "algo@algo.com", haveCertification: true},
+            { name: "Fuentes", email: "algo@algo.com", haveCertification: false}
+        ];
+        const studentsWithCertification = StudentService.getStudentsByCertification(students);
+        expect(studentsWithCertification).toContain("algo@algo.com");
+    });
 });
