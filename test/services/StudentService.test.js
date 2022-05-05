793ad83d7305d4327e8c7aaa2cd0050db05a15ef
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-05-05 14:33:03 -0500

    Adding the getStudentsByCredits method to the StudentService class and its respective tests

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -40,7 +40,7 @@ describe("Tests for student services", () => {
         const studentsWithCertification = StudentService.getStudentsByCertification(students);
         expect(studentsWithCertification).not.toEqual([{ name: "Lucinda", email: "algo@algo.com", haveCertification: true}]);
     });
-    test("5) Return email of students with certification == true", () => {
+    test("6) Return email of students with certification == true", () => {
         const students = [
             { name: "Warren", email: "algo@algo.com", haveCertification: false},
             { name: "Lucinda", email: "algo@algo.com", haveCertification: true},
@@ -48,5 +48,33 @@ describe("Tests for student services", () => {
         ];
         const studentsWithCertification = StudentService.getStudentsByCertification(students);
         expect(studentsWithCertification).toContain("algo@algo.com");
+    });
+    test("7) Return students with 500 or more credits", () => {
+        const students = [
+            { name: "Warren", credits: 500},
+            { name: "Lucinda", credits: 400},
+            { name: "Fuentes", credits: 600}
+        ];
+        const studentsCredits = students.filter(studentCredit => studentCredit.credits >= 500);
+        expect(studentsCredits.length).toBe(2);
+    });
+    test("8) Return students name with 500 or more credits", () => {
+        const students = [
+            { name: "Warren", credits: 500},
+            { name: "Lucinda", credits: 400},
+            { name: "Fuentes", credits: 600}
+        ];
+        const studentsCredits = students.filter(studentCredit => studentCredit.credits >= 500).map(studentShortData => { return { name: studentShortData.name };});
+        expect(studentsCredits).toStrictEqual([{name: "Warren"}, {name: "Fuentes"}]);
+    });
+    test("9) Return Students name and credits if the credits are 500 or more", () => {
+        const studens = Reader.readJsonFile("test/students.json");
+        const studentsByCredits = StudentService.getStudentsByCredits(studens);
+        expect(studentsByCredits).toHaveLength(2);
+    });
+    test("10) Return Students name and credits if the credits are 500 or more", () => {
+        const studens = Reader.readJsonFile("test/students.json");
+        const studentsByCredits = StudentService.getStudentsByCredits(studens);
+        expect(studentsByCredits).toStrictEqual([{name: "Warren", credits: 508}, {name: "Lucinda", credits: 677}]);
     });
 });
